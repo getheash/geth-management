3b60bf8e1b102a465a57614c489ce80cc4e85e52
Geth <mashven@gmail.com>
2020-10-02 18:32:42 +0530

    Implemented Title component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "./component/navbar";
+import Title from "./component/title";
 
 const App = () => {
   return (
     <React.Fragment>
       <NavBar />
+      <main className="container">
+        <Title />
+      </main>
     </React.Fragment>
   );
 };
