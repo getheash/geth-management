1f16166f500c46112bb68ee15c9b9fde9bbc4a92
Geth <mashven@gmail.com>
2020-10-09 22:44:14 +0530

    features component implementation incomplete

--- src/component/features.jsx
+++ src/component/features.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Features = () => {
   return (
-    <div id="features">
-      <p id="intro">
-        Are you tired of people posting your copyright content online without
-        your consent? Don't you worry because we got your back.
-      </p>
+    <div>
+      <section id="features">
+        <p id="intro">
+          Are you tired of people posting your copyright content online without
+          your consent? Don't you worry because we got your back.
+        </p>
+      </section>
     </div>
   );
 };
