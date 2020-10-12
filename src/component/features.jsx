8a09d5c84ce0a4065542dadfaf2a8f7ccc64b48f
Geth <mashven@gmail.com>
2020-10-12 20:10:34 +0530

    Implemented Features component
    responsive behavior still pending

--- src/component/features.jsx
+++ src/component/features.jsx
@@ -8,6 +8,60 @@ const Features = () => {
           Are you tired of people posting your copyright content online without
           your consent? Don't you worry because we got your back.
         </p>
+        <div className="feature">
+          <div className="feature-box">
+            <div className="step">
+              Step 1:
+              <span className="icon">
+                <i className="fas fa-search"></i>
+              </span>
+            </div>
+            <div className="description">
+              <p>
+                We search for copyright infringement of your content on the
+                world wide web with various tools at our disposal. If there is a
+                place where your copyright content been posted without your
+                consent, we will find it.
+              </p>
+            </div>
+          </div>
+          <div className="feature-box">
+            <div className="step">
+              Step 2:
+              <span className="icon">
+                <i className="fas fa-window-close"></i>
+              </span>
+            </div>
+            <div className="description">
+              <p>
+                We send DMCA takedown requests for copyright infringement so
+                that you don't have to worry about people accessing them. Once
+                we have successfully taken down the content for copyright
+                infringement, we will notify you with email confirmation.
+              </p>
+            </div>
+          </div>
+          <div className="feature-box">
+            <div className="step">
+              Step 3:
+              <span className="icon">
+                <i className="fas fa-briefcase-medical"></i>
+              </span>
+            </div>
+            <div className="description">
+              <p>
+                We continuously monitor the internet for any copyright
+                infringement while you can have a piece of mind and spend your
+                valuable time as you please. You can also submit links for
+                takedown using{" "}
+                <a className="text-dark" href="#submitLinks">
+                  Submit Links
+                </a>{" "}
+                section
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
