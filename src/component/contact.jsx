516fdd40894b42c14a7d36e7155fe66c172aa801
Geth <mashven@gmail.com>
2020-10-13 08:57:04 +0530

    added email envelope icon in contact component

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -6,8 +6,13 @@ const Contact = () => {
       <section id="contact" className="border border-white">
         <h3>Contact us</h3>
         <p>
-          If you need more information or do you have any queries, please
-          contact us at this email: We look forward to hear from you.
+          If you need more information or do you have any queries about how we
+          could protect your content, please contact us at{" "}
+          <i style={{ color: "dodgerblue" }} className="fas fa-envelope"></i> :
+        </p>
+        <p>
+          If you are a subscriber, please mention your idenity code in the
+          subject line for quick assistance. We look forward to hear from you.
         </p>
       </section>
     </div>
