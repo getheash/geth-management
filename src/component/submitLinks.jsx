8fb6ca169ba056f27bf5ec572af7e16f9edcf5ea
Geth <mashven@gmail.com>
2020-11-11 23:58:49 +0530

    removed netlify-recaptcha for now

--- src/component/submitLinks.jsx
+++ src/component/submitLinks.jsx
@@ -12,7 +12,6 @@ const SubmitLinks = () => {
           name="submit-links"
           id="submitLinksForm"
           method="post"
-          data-netlify-recaptcha="true"
         >
           <input type="hidden" name="form-name" value="submit-links" />
           <div className="form-group">
@@ -66,7 +65,6 @@ const SubmitLinks = () => {
               placeholder="Enter your comments..."
             ></textarea>
           </div>
-          <div data-netlify-recaptcha="true"></div>
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
