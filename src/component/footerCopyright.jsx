600366bfb7c3b4dd29f5c3d3552336d999b53f2b
Geth <mashven@gmail.com>
2021-01-03 00:00:48 +0530

    added 2021 to the footer

--- src/component/footerCopyright.jsx
+++ src/component/footerCopyright.jsx
@@ -4,7 +4,7 @@ const FooterCopyright = () => {
   return (
     <div style={{ height: "80px" }}>
       <div className="footer-copyright h-100 w-100 text-center text-white">
-        &copy; Copyright 2020{" "}
+        &copy; Copyright 2020 - 2021{" "}
         <a href="https://www.gethmanagement.com/" className="text-white">
           Geth Management
         </a>{" "}
